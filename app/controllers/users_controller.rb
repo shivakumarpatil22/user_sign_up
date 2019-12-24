926b28a5c5e49750bc40c8beb845269d5bf74cf7
Shivakumar <shivakumar@ip-192-168-5-45.ap-south-1.compute.internal>
2019-12-24 18:19:38 +0530

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :destroy, :update]
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+    @users = User.all.paginate(page: params[:page],per_page: 10)
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'user was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'user was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path 
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end 
+end
